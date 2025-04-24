73d8a38543ed44d4ff6e3f3e5895a0536f1361ad
TheBitty <hitsuu23@gmail.com>
2025-04-23 23:44:00 -0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+#include <thread>
+
+int main() {  
+	//we are first have to get our offsets 
+	
+// we will have to find everyone in the game our team/ops and find their position to our croshairs in (x,y,z) 
+//            constexpr std::ptrdiff_t dwEntityList = 0x1A292F0;
+
+//
+
+	return 0; }
